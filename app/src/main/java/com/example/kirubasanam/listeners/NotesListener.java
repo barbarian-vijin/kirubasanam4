b20a548842231b0848a87a63d801d0b89efe4589
Sibin Yovel <vijiny9629@gmail.com>
2021-12-03 16:43:53 +0530

    notespage updated

---
+++ app/src/main/java/com/example/kirubasanam/listeners/NotesListener.java
@@ -0,0 +1,8 @@
+package com.example.kirubasanam.listeners;
+
+import com.example.kirubasanam.entities.Note;
+
+public interface NotesListener {
+
+    void  onNoteClicked(Note note , int position);
+}
